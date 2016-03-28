6ce944748ad31d80aadccb0efafe73f3ecb195a7
Mingcai SHEN <archsh@gmail.com>
2016-03-28 20:54:09 +0800

    Add rsyslog and docker-entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,15 @@ MAINTAINER  "Mingcai SHEN <archsh@gmail.com>"
 ENV HAPROXY_VERSION 1.5.14.Rev1
 
 VOLUME /etc/haproxy 
+VOLUME /var/log
+COPY docker-entrypoint.sh /
 
 RUN yum install -y /sbin/service  \
     && rpm -Uvh https://github.com/extvos/rpmbuild/raw/master/RPMS/x86_64/haproxy-1.5.14.Rev1-1.el6.x86_64.rpm \
+    && yum install -y rsyslog \
+    && chmod +x /docker-entrypoint.sh \
     && mkdir -p /var/run/haproxy/ && mkdir -p /usr/local/share/haproxy 
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
 
 CMD ["haproxy", "-f", "/etc/haproxy/haproxy.cfg"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+# define help message
+show_help() {
+    echo """
+    Usage: docker run [OPTIONS] <imagename> [COMMAND]
+
+    Commands
+    bash     : Start a bash shell
+    help     : Show this message
+
+    """
+}
+
+# Initialize
+initialize() {
+    echo "Initializing ..."
+}
+
+# Start tasks
+main_task() {
+	/etc/init.d/rsyslog start
+}
+
+# Run
+case "$1" in
+    bash)
+        /bin/bash "${@:2}"
+    ;;
+    help)
+        show_help
+    ;;
+    *)
+        initialize
+        main_task
+        exec "$@"
+    ;;
+esac
+
